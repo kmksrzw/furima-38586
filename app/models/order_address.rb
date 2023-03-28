6d6d5ebcaa16b23f1d50ad66e75916aceb77d9f3
kmksrzw <kmkist@outlook.jp>
2023-03-28 16:45:20 +0900

    任意修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :tel, :user_id, :item_id
-  attr_accessor :token
+  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :tel, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
